ad897851e7213013ec862aa026e993f42e6baa1b
Josef1923 <schererjos01@gmail.com>
2024-08-12 10:59:20 +0200

    Supression des (on clicks) de l'HTML

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,23 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
-}
+// DOM Elements
+const modal = document.getElementById("contact_modal"); 
+const contactButton = document.getElementById("contactButton"); 
+const closeButton = document.getElementById("closeButton"); 
 
+// Event pour ouvrir le modal
+contactButton.addEventListener("click", openModal);
+
+// Fonction pour ouvrir le modal
+function openModal() {
+    modal.style.display = "block";
+    modal.setAttribute("aria-hidden", "false"); 
+  }
+
+// Event pour fermer le modal
+closeButton.addEventListener("click", closeModal);
+
+// Fonction pour fermer le modal
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
-}
+    modal.setAttribute("aria-hidden", "true"); 
+  }
+  
