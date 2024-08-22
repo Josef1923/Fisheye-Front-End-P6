b88944e2bf97ce4c62dc120c9e7c4bf6f7b89b16
Josef1923 <schererjos01@gmail.com>
2024-08-22 10:43:57 +0200

    changement L11 photgraphers.js (passage de "==" à "===")

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,14 +1,14 @@
 // Récupère l'ID du photographe depuis l'URL
 function getPhotographerId() {
     const params = new URLSearchParams(window.location.search);
-    return params.get('id');
+    return parseInt(params.get('id'));
 }
 
 // Récupère les données du photographe par son ID
 async function getPhotographerData(id) {
     const response = await fetch('data/photographers.json');
     const { photographers } = await response.json();  //destructuration objet photographer grace aux {}
-    return photographers.find(photographer => photographer.id == id);
+    return photographers.find(photographer => photographer.id === id);
 }
 
 // Initialisation
