6f8302b61b7596229a8f89dde429919bde1d517d
Josef1923 <schererjos01@gmail.com>
2024-08-27 14:32:20 +0200

    Ajout script fermeture lightbox

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -3,7 +3,7 @@
  */
 function lightboxOpener() {
     const lightbox = document.querySelector('dialog.light-box');
-    lightbox.style.display = 'block';
+    lightbox.style.display = 'flex';
     lightbox.setAttribute('aria-hidden', 'false');
 }
 
@@ -19,11 +19,30 @@ function openListener () {
 } 
 
 /**
+ * function de fermeture lightbox
+ */
+function lightboxCloser() {
+    const lightbox = document.querySelector('dialog.light-box');
+    lightbox.style.display = '';
+    lightbox.setAttribute('aria-hidden', 'true');
+}
+
+/**
+ * fonction de listener de fermeture lightbox
+ */
+function closeListener() {
+    const closer = document.getElementById('close-slide');
+
+    closer.addEventListener('click', lightboxCloser);
+}
+
+/**
  * Initialise lightbox 
  */
 async function initMediaGallery() {
     await mediaGalleryByID();
     openListener();
+    closeListener();
 }
 
 initMediaGallery();
